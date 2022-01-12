1fce59088e41fcf955207cc9dc00d44f91242041
x-Sherman <x@xsherman.com>
2022-01-12 23:30:31 +0300

    Presentation layer

---
+++ lib/weather/view/weather_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_bloc/search/view/search_page.dart';
+import 'package:weather_app_bloc/settings/view/settings_page.dart';
+import 'package:weather_app_bloc/theme/cubit/theme_cubit.dart';
+import 'package:weather_app_bloc/weather/cubit/weather_cubit.dart';
+import 'package:weather_app_bloc/weather/widgets/widgets.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+class WeatherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => WeatherCubit(context.read<WeatherRepository>()),
+      child: WeatherView(),
+    );
+  }
+}
+
+class WeatherView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather App'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.of(context).push<void>(
+                SettingsPage.route(
+                  context.read<WeatherCubit>(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: BlocConsumer<WeatherCubit, WeatherState>(
+          listener: (context, state) {
+            if (state.status.isSuccess) {
+              context.read<ThemeCubit>().updateTheme(state.weather);
+            }
+          },
+          builder: (context, state) {
+            switch (state.status) {
+              case WeatherStatus.initial:
+                return const WeatherEmpty();
+              case WeatherStatus.loading:
+                return const WeatherLoading();
+              case WeatherStatus.success:
+                return WeatherPopulated(
+                  weather: state.weather,
+                  units: state.temperatureUnits,
+                  onRefresh: () {
+                    return context.read<WeatherCubit>().refreshWeather();
+                  },
+                );
+              case WeatherStatus.failure:
+              default:
+                return const WeatherError();
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.search),
+        onPressed: () async {
+          final city = await Navigator.of(context).push(SearchPage.route());
+          await context.read<WeatherCubit>().fetchWeather(city);
+        },
+      ),
+    );
+  }
+}
