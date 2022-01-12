1b775b28d57fb05475eecc143ccee415c667add7
x-Sherman <x@xsherman.com>
2022-01-12 19:53:28 +0300

    MetaWeather Api Client

---
+++ packages/meta_weather_api/lib/src/meta_weather_api_client.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:meta_weather_api/meta_weather_api.dart';
+
+class LocationIdRequestFailure implements Exception {}
+
+class LocationNotFoundFailure implements Exception {}
+
+class WeatherRequestFailure implements Exception {}
+
+class WeatherNotFoundFailure implements Exception {}
+
+class MetaWeatherApiClinet {
+  MetaWeatherApiClinet({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+
+  static const _baseUrl = 'www.metaweather.com';
+  final http.Client _httpClient;
+
+  Future<Location> locationSearch(String query) async {
+    final locationRequest = Uri.https(
+      _baseUrl,
+      '/api/location/search',
+      <String, String>{'query': query},
+    );
+    final locationResponse = await _httpClient.get(locationRequest);
+
+    if (locationResponse.statusCode != 200) {
+      throw LocationIdRequestFailure();
+    }
+
+    final locationJson = jsonDecode(
+      locationResponse.body,
+    ) as List;
+
+    if (locationJson.isEmpty) {
+      throw LocationNotFoundFailure();
+    }
+
+    return Location.fromJson(locationJson.first as Map<String, dynamic>);
+  }
+
+  Future<Weather> getWeather(int locationId) async {
+    final weatherRequest = Uri.https(_baseUrl, '/api/location/$locationId');
+    final weatherResponse = await _httpClient.get(weatherRequest);
+
+    if (weatherResponse.statusCode != 200) {
+      throw WeatherRequestFailure();
+    }
+
+    final bodyJson = jsonDecode(weatherResponse.body) as Map<String, dynamic>;
+
+    if (bodyJson.isEmpty) {
+      throw WeatherNotFoundFailure();
+    }
+
+    final weatherJson = bodyJson['consolidated_weather'] as List;
+
+    if (weatherJson.isEmpty) {
+      throw WeatherNotFoundFailure();
+    }
+
+    return Weather.fromJson(weatherJson.first as Map<String, dynamic>);
+  }
+}
